d7f76fce5720a882edcddb22111b5da5808c8854
Strroke21 <rishipatil1707@gmail.com>
2025-01-16 11:21:20 +0530

    new file

---
+++ install_mavlink_rs.sh
@@ -0,0 +1,14 @@
+
+
+# clone the the git repo for mavlink-rs
+
+git clone https://github.com/elmopl/mavlink-rs.git
+
+# build the crate
+
+cd mavlink-rs
+cargo build --release
+
+#include compiled library with rustc
+
+rustc -L path/to/compiled/library your_program.rs --extern mavlink=path/to/compiled/mavlink.rlib
